dd8a8aa5b6b5c65c03d181913b14b5d3a0ecd6cd
abc <abc@gmail.com>
2020-06-30 19:53:05 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular13';
+export class AppComponent implements OnInit{
+  form = new FormGroup({
+    firstName: new FormControl('',Validators.required),
+    lastName: new FormControl('',Validators.required),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.minLength(6)]),
+    city: new FormControl('',Validators.required),
+    district: new FormControl('',Validators.required),
+    zip: new FormControl('',Validators.required)
+
+  })
+  constructor(){}
+  
+  
+    
+  ngOnInit(){
+  }
+  onSubmit(){
+    alert(JSON.stringify(this.form.value))
+  }
 }
